3e1d352f7e00df37295d23fd35f752113eb98051
Ritobroto Maitra <ritobrotomaitra@gmail.com>
2015-06-12 06:01:35 +0100

    s01c01 - before looping. working for manual input.

--- set1/s01c01.c
+++ set1/s01c01.c
@@ -45,7 +45,10 @@ int main() {
 	int sizeCheck = inputSize % 3;
 
 	uint8_t hex[6] = {0};
-	uint8_t base64[4] = {0};
+	uint8_t hex2[3] = {3};
+	int sizeOfBase64 = 4 * (inputSize/3);
+	uint8_t base64[10800] = {0};
+	char base64char[10800];
 
 	// if the size is perfectly divisible by 3, then take the blocks one by one.
 
@@ -56,14 +59,52 @@ int main() {
 				if(isdigit(input[j])) {
 					hex[j] = (uint8_t)((int)input[j] - 48);
 				}
-				else if(isxdigit(input[j])) {
-					hex[j] = (uint8_t)((int)input[j] - 97);
+				else if(isalpha(input[j])) {
+					hex[j] = (uint8_t)((int)input[j] - 87);
 				}
 				else {
 					die("The system encountered an error.");
 				}
 
 			}
+
+			// Now let us fuse two hex characters together - 8 bits, to form our octet.
+
+			hex2[0] = (hex[1] | (hex[0] << 4));
+			hex2[1] = (hex[3] | (hex[2] << 4));
+			hex2[2] = (hex[5] | (hex[4] << 4));
+
+			// Now we have the 3 octets.
+
+			base64[0] = hex2[0] >> 2;
+			base64[1] = (hex[2] | ((hex[1] & 3) << 4));
+			hex[4] = hex[4] >> 2;
+			hex[4] = hex[4] << 2;
+			hex[3] = hex[3] << 4;
+			base64[2] = (hex[3] | hex[4])>>2;
+			base64[3] = hex2[2] & 0x3f;
+
+			printf("%d, %d\n", hex[3], base64[2]);
+
+			for(j=0; j<4; j++){
+				if(base64[j] < 26) {
+					base64char[j] = (char)(base64[j] + 65);
+				}
+				else if(base64[j] < 52) {
+					base64char[j] = (char)(base64[j] + 71);
+				}
+				else if(base64[j] < 62) {
+					base64char[j] = (char)(base64[j] - 4);
+				}
+				else if (base64[j] == 62) {
+					base64char[j] = '+';
+				}
+				else {
+					base64char[j] = '/';
+				}
+			}
+
+			printf("%c %c %c %c\n", base64char[0], base64char[1], base64char[2], base64char[3] );
 
 		}
 	}
